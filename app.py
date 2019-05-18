8e73d68968dea2fcb968f277bad9cb892b5d258a
dmitrisalov <dmitrisalov98@gmail.com>
2019-05-18 16:30:23 -0500

    Finished home page

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html", title="Home")
 
-@app.route("/old")
-def temp():
-    return render_template("old_home.html", title="Home")
+@app.route("/pong")
+def pong():
+    return render_template("home.html", title="Pong")
+
+@app.route("/infinity-cube")
+def infinity_cube():
+    return render_template("home.html", title="Infinity Cube")
 
 # For dev environment purposes
 if __name__ == "__main__":
