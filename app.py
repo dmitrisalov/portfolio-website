e9bda2080ced779d9f40dbbcdd60fdca4f44b952
dmitrisalov <dmitrisalov98@gmail.com>
2019-05-18 01:15:12 -0500

    Restyled website

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 # Home page
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template("home.html", title="Home")
+
+@app.route("/old")
+def temp():
+    return render_template("old_home.html", title="Home")
 
 # For dev environment purposes
 if __name__ == "__main__":
--- static/js/main.js
+++
@@ -1,4 +0,0 @@
-// Make the header fit the window size.
-$(document).ready(function(){
-        $('.header').height($(window).height());
-    })
